49d6006ae1fefc042004f35b902bef17933eca14
saeyama <camper_green_yellow_21@yahoo.co.jp>
2021-10-16 15:06:45 +0900

    picture(carrierwave)追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_16_035724) do
+ActiveRecord::Schema.define(version: 2021_10_16_055314) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_10_16_035724) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
+    t.string "picture"
   end
 
 end
